8d4ef176bd6328d8bb17491eecfd07c2b9c691c1
Absurd1ty <63378520+Absurd1ty@users.noreply.github.com>
2020-10-21 17:38:05 -0700

    Update singleNumber.py

--- singleNumber.py
+++ singleNumber.py
@@ -1,7 +1,8 @@
-import numpy as np
+
 """Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
 Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?"""
+
 
 
 class Solution:
@@ -11,6 +12,3 @@ class Solution:
         subarray1 = nums[0, length]
         subarray2 = nums[length+1, -1]
         return abs(sum(subarray1) - sum(subarray2))
-
-result = Solution().singleNumber([1,1,2,2,3,3,4])    
-print(result)   
