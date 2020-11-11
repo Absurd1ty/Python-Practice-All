d432197fb1f778d5797ec7486cda8600e8aab81f
Absurd1ty <63378520+Absurd1ty@users.noreply.github.com>
2020-11-10 17:42:38 -0800

    Update testing.py

--- 5problems_pt5/testing.py
+++ 5problems_pt5/testing.py
@@ -1,26 +1,14 @@
-"""output = []
-
-for row in range(1, ):
-            new_row = [1]*row
-            print(row)
-            print (new_row)
-            
-            for j in range(1, row-1):
-                new_row[j] = output[-1][j-1] + output[-1][j] 
-            
-            output.append(new_row)
-print(new_row)
-print (output)"""
-
 
 pascal_triangle = []
 num_rows = 5
 for row in range(1, 5+1):
     new_row = [1]*row
+    print(new_row)
     pascal_triangle.append(new_row)
-
+    for place in range(1, row-1):
+         print(place)
+        new_row[place]=pascal_triangle[-1][place-1] + pascal_triangle[-1][place] 
+    pascal_triangle.append(new_row)
 
 print(new_row)
 print(pascal_triangle)
-
-
